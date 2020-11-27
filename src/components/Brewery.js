616e9c4346adeab4e245bd9c50f80cd3d98ff8d4
x5se7en9x <tnansci@gmail.com>
2020-11-27 18:32:19 -0500

    Commented out Loading component in Brewery.js

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -15,33 +15,33 @@ const Brewery = (props) => {
             .catch(error => console.error(error))
     }, [])
 
-    // if (!brewery) {
-    //     return <Loading />
-    // } else {
-    //     const { id, name, website_url, brewery_type, phone, street, city, state, postal_code, country } = brewery
-    //     let formatedPhone
-    //     if (phone) {
-    //         formatedPhone = phone.split('')
-    //         formatedPhone.unshift("(")
-    //         formatedPhone.splice(4, 0, ") ")
-    //         formatedPhone.splice(8, 0, "-")
-    //     }
+    if (!brewery) {
+        return <Loading />
+    } else {
+        const { id, name, website_url, brewery_type, phone, street, city, state, postal_code, country } = brewery
+        let formatedPhone
+        if (phone) {
+            formatedPhone = phone.split('')
+            formatedPhone.unshift("(")
+            formatedPhone.splice(4, 0, ") ")
+            formatedPhone.splice(8, 0, "-")
+        }
 
-    //     return (
-    //         <div className='brewery-container'>
-    //             <h3>{name}</h3>
-    //             <p>{formatedPhone}</p>
-    //             <p><a href={website_url} target='_blank'>{website_url}</a></p>
-    //             <div className='address-container'><p>Address</p>
-    //                 <p>{street}</p>
-    //                 <p>{city}, {state}</p>
-    //                 <p>{postal_code}</p>
-    //             </div>
-    //             <button onClick={() => history.goBack()}>back</button>
-    //         </div>
-    //     )
-    // }
-    return <Loading />
+        return (
+            <div className='brewery-container'>
+                <h3>{name}</h3>
+                <p>{formatedPhone}</p>
+                <p><a href={website_url} target='_blank'>{website_url}</a></p>
+                <div className='address-container'><p>Address</p>
+                    <p>{street}</p>
+                    <p>{city}, {state}</p>
+                    <p>{postal_code}</p>
+                </div>
+                <button onClick={() => history.goBack()}>back</button>
+            </div>
+        )
+    }
+    // return <Loading />
 
 }
 
