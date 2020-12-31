849623fc7a85927b42b169d018711c72c8c14bb2
x5se7en9x <tnansci@gmail.com>
2020-12-31 16:10:11 -0500

    Added some information about dev and the app in the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,17 @@ import React from 'react'
 const About = () => {
     return (
         <div className="about-page">
-            <h3>About page!</h3>
+            <h3>About Open Tabs:</h3>
+            <p>Open Tabs is currently a front end react application. The app capable of fetching brewery data from a third party API - BreweryDB, using keyword(search term).
+            </p>
+            <p>Keyword can be a name or a location.
+            </p>
+            <br />
 
+            <h3>About Me - the junior deveoper :D</h3>
+            <p>I am a junior level software developer who wants to spread his wings and soars in to the sky. In order to do that, first he needs to find a sturdy base for his first footing - first software development position.
+            </p>
+            <h2>Any helps will be greatly appreciated!!</h2>
         </div>
     )
 }
